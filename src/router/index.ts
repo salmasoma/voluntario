bb2b66e38fae794b0eefa8e253424a3363a68e60
Karim <karim.wael@gmail.com>
2022-03-08 23:26:58 +0400

    register + Login pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,17 @@
 import { createWebHistory, createRouter } from "vue-router";
 import register from "../pages/register.vue";
+import login from "../pages/login.vue";
 
 const routes = [
   {
     path: "/register",
     name: "Register",
     component: register,
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: login,
   },
 ];
 
