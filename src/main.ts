274b6ed9be9c47b11cbe82af1381910993b5e6f0
Salma Hassan <salmasoma@gmail.com>
2022-03-13 22:31:27 +0400

    yalla

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router/index";
 import "./index.css";
+import 'v-calendar/dist/style.css';
 
 createApp(App).use(router).mount("#app");
